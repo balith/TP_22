b92a1e6aeb5e3c0d695af3f70b3316a326fb2dbb
balith <balith@educ.college-montreal.qc.ca>
2022-10-13 09:16:58 -0400

    secind comit

--- TP_2.py
+++ TP_2.py
@@ -1,23 +1,27 @@
 
 import random
 
-n = random.randint(0,100)
+guess = random.randint(0, 100)
 appreciation = "?"
-nrb = 0
+nbr = 0
 print("devinez le nombre que je pense(indice il est entre 0 et 100)")
+
+
 while True:
     var = input("quell est votre choice")
     var = int(var)
-    if var < n :
-        nbr = +1
-        appreciation = "non, je pensais a un notre chiffre, il est plus grand:"
-        print(appreciation, var)
+    if var < guess :
+        nbr += 1
+        print("non, je pensais a un notre chiffre, il est plus grand:", var)
 
     else :
-        nbr = +1
-        appreciation = "non, je pensai a un notre chiffre, il est plus petit:"
-        print(appreciation, var)
-    if var == n:
+        nbr = nbr + 1
+        print("non, je pensai a un notre chiffre, il est plus petit:", var)
+    if var == guess:
         appreciation ="wow, le nombre etait:"
         print(appreciation, var, "vous avez assaiye:", nbr)
-        break
+
+
+
+
+
